043044e40257a5535fbf091cdd87bf153369949b
Calista Nasser <nasser.calista@gmail.com>
2017-12-20 21:56:25 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray() {
+  
+}
+function destructiveAddElementToBeginningOfArray() {
+  
+}
+function addElementToEndOfArray() {
+  
+}
+function destructiveAddElementToEndOfArray() {
+  
+}
+function accessElementInArray() {
+  
+}
+function destructiveRemoveElementFromBeginningOfArray() {
+  
+}
+function removeElementFromBeginningOfArray() {
+  
+}
+function destructiveRemoveElementFromEndOfArray() {
+  
+}
+function removeElementFromEndOfArray() {
+  
+}
